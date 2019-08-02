db538ee2696c0edf6f0a6fb34f048b49259110be
Gingertonic <bethmschofield@gmail.com>
2019-08-02 22:38:46 +0100

    Updates

---
+++ precache-manifest.e1c688cc4e575ff4fd5121e718c5118d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c3ee94822b0355d8d469e9257122f6",
+    "url": "/alpha-beta/index.html"
+  },
+  {
+    "revision": "ffab0c47079aa719fdfe",
+    "url": "/alpha-beta/static/css/main.588a7650.chunk.css"
+  },
+  {
+    "revision": "8f074d9270ba2d170f7e",
+    "url": "/alpha-beta/static/js/2.9d556c1d.chunk.js"
+  },
+  {
+    "revision": "ffab0c47079aa719fdfe",
+    "url": "/alpha-beta/static/js/main.c18d3b19.chunk.js"
+  },
+  {
+    "revision": "47089fb1cec11376f8c1",
+    "url": "/alpha-beta/static/js/runtime~main.984e4158.js"
+  }
+]);
